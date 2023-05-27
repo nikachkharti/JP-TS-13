6a981e2b16afb266dae91420aef5b83f0d055300
Nikoloz Chkhartishvili <nika.chkhartishvili7@gmail.com>
2023-05-27 16:38:18 +0400

    Vehicles model created

---
+++ JP-TS-13/JP-TS-13-MainConnsole/Vehicle.cs
@@ -0,0 +1,15 @@
+﻿namespace JP_TS_13_MainConnsole
+{
+    public class Vehicle
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public byte Cylinder { get; set; }
+        public float Engine { get; set; }
+        public string Drive { get; set; }
+        public string Transmission { get; set; }
+        public byte City { get; set; }
+        public byte Combined { get; set; }
+        public byte Highway { get; set; }
+    }
+}
