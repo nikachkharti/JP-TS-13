1bdd0b036981f5ca1c8b43eb19a40af27c14eb34
Nikoloz Chkhartishvili <nika.chkhartishvili7@gmail.com>
2023-06-01 16:20:59 +0400

    Algorithms class added

---
+++ JP-TS-13/JP-TS-13-MainConnsole/Algorithms.cs
@@ -0,0 +1,18 @@
+﻿namespace JP_TS_13_MainConnsole
+{
+    public static class Algorithms
+    {
+        public static Vehicle[] Select(string[] collection)
+        {
+            Vehicle[] result = new Vehicle[collection.Length];
+            for (int i = 0; i < collection.Length; i++)
+            {
+                result[i] = Vehicle.Parse(collection[i]);
+            }
+
+            return result;
+        }
+
+
+    }
+}
--- JP-TS-13/JP-TS-13-MainConnsole/Program.cs
+++ JP-TS-13/JP-TS-13-MainConnsole/Program.cs
@@ -1,15 +1,12 @@
 ﻿using JP_TS_13_MainConnsole;
 
 string[] rawData = File.ReadAllLines(@"../../../vehicles.csv");
-
-Vehicle[] vehicles = new Vehicle[rawData.Length];
-for (int i = 0; i < rawData.Length; i++)
-{
-    vehicles[i] = Vehicle.Parse(rawData[i]);
-}
-
-
+Vehicle[] parsedCars = Algorithms.Select(rawData);
 
 
 
 Console.ReadLine();
+
+
+
+
