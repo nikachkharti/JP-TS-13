c3c6df8520e3d8b7c95dcb5c1e6875f74aee4696
Nikoloz Chkhartishvili <nika.chkhartishvili7@gmail.com>
2023-06-22 16:54:33 +0400

    Customer model created

---
+++ JP-TS-13/JPTS13Bank/Models/Customer.cs
@@ -0,0 +1,32 @@
+﻿namespace JPTS13Bank.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string IdentityNumber { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public CustomerType Type { get; set; }
+    }
+
+    internal static class CustomerExtension
+    {
+        internal static Customer ToCustomer(this string input)
+        {
+            string[] csvInput = input.Split(',');
+
+            Customer result = new();
+            result.Id = int.Parse(csvInput[0]);
+            result.Name = csvInput[1];
+            result.IdentityNumber = csvInput[2];
+            result.PhoneNumber = csvInput[3];
+            result.Email = csvInput[4];
+            result.Type = (CustomerType)Enum.Parse(typeof(CustomerType), csvInput[5]);
+
+            return result;
+        }
+    }
+
+
+}
---
+++ JP-TS-13/JPTS13Bank/Models/CustomerType.cs
@@ -0,0 +1,8 @@
+﻿namespace JPTS13Bank.Models
+{
+    public enum CustomerType
+    {
+        Phyiscial = 0,
+        Legal = 1
+    }
+}
