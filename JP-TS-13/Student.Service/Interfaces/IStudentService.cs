ded235b117d3d7a4751b050d9a597e37ffc6c199
Admin <Admin@T-25-01>
2023-10-12 16:52:15 +0400

    Task added

--- JP-TS-13/Student.Service/Interfaces/IStudentService.cs
+++ JP-TS-13/Student.Service/Interfaces/IStudentService.cs
@@ -5,5 +5,12 @@ namespace Student.Service
     public interface IStudentService
     {
         List<StudentModel> GetAllStudents();
+
+        //TODO - სტუდენტების ცხრილის მსგავსად შექმენით მასწავლებლების ცხრილი ზუსტად შემდეგი პარამეტრებით
+        //FirstName
+        //LastName
+        //DateOfBirth
+        //Pin
+        //Salary
     }
 }
