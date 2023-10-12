3eca5e728955f8032e2153164a2a372ce107e356
Admin <Admin@T-25-01>
2023-10-12 17:48:42 +0400

    TODO task added

--- JP-TS-13/Student.Service/Interfaces/IStudentService.cs
+++ JP-TS-13/Student.Service/Interfaces/IStudentService.cs
@@ -7,6 +7,7 @@ namespace Student.Service
         List<StudentModel> GetAllStudents();
 
         //TODO - სტუდენტების ცხრილის მსგავსად შექმენით მასწავლებლების ცხრილი ზუსტად შემდეგი პარამეტრებით
+        //Id PRIMART KEY   
         //FirstName
         //LastName
         //DateOfBirth
