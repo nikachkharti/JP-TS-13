5046c2ec519635e6fbb90ed6d901d1ccfaf8ed23
Admin <Admin@T-25-01>
2023-11-16 17:54:02 +0400

    API Intro

---
+++ JP-TS-13/Employees.API/Controllers/EmployeesController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Employees.API.Controllers
+{
+    [ApiController]
+    [Route("employees")]
+    public class EmployeesController : ControllerBase
+    {
+        private List<Employee> employees = new();
+        public EmployeesController()
+        {
+            employees.Add(
+                    new Employee()
+                    {
+                        Id = 1,
+                        FirstName = "Nika",
+                        LastName = "Chkhartishvili"
+                    }
+                );
+
+            employees.Add(
+                 new Employee()
+                 {
+                     Id = 2,
+                     FirstName = "Giorgi",
+                     LastName = "Giorgadze"
+                 }
+            );
+
+            employees.Add(
+                new Employee()
+                {
+                    Id = 3,
+                    FirstName = "Davit",
+                    LastName = "Davitidze"
+                }
+            );
+
+        }
+
+        [HttpGet]
+        public List<Employee> GetEmployees()
+        {
+            return employees;
+        }
+
+        [HttpGet("{id}")]
+        public Employee GetEmployee(int id)
+        {
+            var result = employees.Find(x => x.Id == id);
+            return result;
+        }
+
+
+        [HttpGet("{lName}/{id}")]
+        public Employee GetEmployeeWithDze(string lName, int id)
+        {
+            var result = employees
+                .Where(x => x.LastName.EndsWith(lName))
+                .FirstOrDefault(x => x.Id == id);
+
+            return result;
+        }
+
+        [HttpPost]
+        public void AddNewEmployee(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ JP-TS-13/Employees.API/Employee.cs
@@ -0,0 +1,9 @@
+﻿namespace Employees.API
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+    }
+}
