9fc30731a61ca1d89bf9d5c1982b067b2867e018
Admin <Admin@T-25-01>
2023-11-18 17:54:20 +0400

    Request response explained

--- JP-TS-13/Employees.API/Controllers/EmployeesController.cs
+++ JP-TS-13/Employees.API/Controllers/EmployeesController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Employees.API.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Employees.API.Controllers
 {
@@ -6,73 +7,101 @@ namespace Employees.API.Controllers
     [Route("employees")]
     public class EmployeesController : ControllerBase
     {
-        private List<Employee> _employees = new()
-        {
-            new Employee
-            {
-                Id = 1,
-                FirstName = "Nika",
-                LastName = "Chkhartishvili"
-            },
-            new Employee
-            {
-                Id = 2,
-                FirstName = "Giorgi",
-                LastName = "Giorgadze"
-            },
-            new Employee
-            {
-                Id = 3,
-                FirstName = "Davit",
-                LastName = "Davitidze"
-            },
-        };
-
         [HttpGet]
-        public List<Employee> GetEmployees()
+        public ActionResult<List<Employee>> GetEmployees()
         {
-            return _employees;
+            var result = EmployeesStore.EmployeeList;
+            return Ok(result);
         }
 
         [HttpGet("{id}")]
-        public Employee GetEmployee(int id)
+        public ActionResult<Employee> GetEmployee(int id)
         {
-            var result = _employees.Find(x => x.Id == id);
-            return result;
-        }
+            if (id <= 0)
+            {
+                return BadRequest();
+            }
 
+            var result = EmployeesStore.EmployeeList.Find(x => x.Id == id);
 
-        [HttpGet("{lName}/{id}")]
-        public Employee GetEmployeeWithDze(string lName, int id)
-        {
-            var result = _employees
-                .Where(x => x.LastName.EndsWith(lName))
-                .FirstOrDefault(x => x.Id == id);
+            if (result == null)
+            {
+                return NotFound(result);
+            }
 
-            return result;
+            return Ok(result);
         }
 
         [HttpPost]
-        public List<Employee> AddNewEmployee(Employee employee)
+        public ActionResult AddNewEmployee(Employee employee)
         {
-            _employees.Add(employee);
-            return _employees;
+            if (employee is null)
+            {
+                return BadRequest(employee);
+            }
+
+            var newId = EmployeesStore.EmployeeList.Max(x => x.Id) + 1;
+            employee.Id = newId;
+
+            EmployeesStore.EmployeeList.Add(employee);
+
+            return Created(string.Empty, employee);
         }
 
         [HttpDelete("{id}")]
-        public void DeleteEmployee(int id)
+        public ActionResult DeleteEmployee(int id)
         {
-            var employee = _employees.Find(x => x.Id == id);
-            _employees.Remove(employee);
+            if (id <= 0)
+            {
+                return BadRequest();
+            }
+
+            var result = EmployeesStore.EmployeeList.Find(x => x.Id == id);
+
+            if (result == null)
+            {
+                return NotFound(result);
+            }
+
+            EmployeesStore.EmployeeList.Remove(result);
+            return NoContent();
         }
 
         [HttpPut]
-        public void UpdateEmployee(int id, Employee newEmployee)
+        public ActionResult UpdateEmployee(int id, Employee model)
         {
-            var employeeToUpdate = _employees.Find(x => x.Id == id);
+            if (id <= 0 || model.Id != id)
+            {
+                return BadRequest();
+            }
 
-            employeeToUpdate.FirstName = newEmployee.FirstName;
-            employeeToUpdate.LastName = newEmployee.LastName;
+            var result = EmployeesStore.EmployeeList.Find(x => x.Id == id);
+
+            if (result == null)
+            {
+                return NotFound(result);
+            }
+
+            result.FirstName = model.FirstName;
+            result.LastName = model.LastName;
+
+            return Ok(result);
         }
+
+
+        //private int MaxId(List<Employee> collection)
+        //{
+        //    int maxElement = collection[0].Id;
+
+        //    foreach (var item in collection)
+        //    {
+        //        if (item.Id > maxElement)
+        //        {
+        //            maxElement = item.Id;
+        //        }
+        //    }
+
+        //    return maxElement;
+        //}
     }
 }
---
+++ JP-TS-13/Employees.API/Data/EmployeesStore.cs
@@ -0,0 +1,12 @@
+﻿namespace Employees.API.Data
+{
+    public static class EmployeesStore
+    {
+        public static List<Employee> EmployeeList { get; set; } = new()
+        {
+            new Employee { Id = 1, FirstName = "Nika", LastName = "Chkhartishvili" },
+            new Employee { Id = 2, FirstName = "Giorgi", LastName = "Giorgadze" },
+            new Employee { Id = 3, FirstName = "Davit", LastName = "Davitidze" }
+        };
+    }
+}
