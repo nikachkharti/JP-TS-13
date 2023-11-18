b36761e1e0cc8d2ca08d8c673eb0606ec50ee8e2
Admin <Admin@T-25-01>
2023-11-18 16:25:48 +0400

    Classwork added

--- JP-TS-13/Employees.API/Controllers/EmployeesController.cs
+++ JP-TS-13/Employees.API/Controllers/EmployeesController.cs
@@ -6,48 +6,38 @@ namespace Employees.API.Controllers
     [Route("employees")]
     public class EmployeesController : ControllerBase
     {
-        private List<Employee> employees = new();
-        public EmployeesController()
+        private List<Employee> _employees = new()
         {
-            employees.Add(
-                    new Employee()
-                    {
-                        Id = 1,
-                        FirstName = "Nika",
-                        LastName = "Chkhartishvili"
-                    }
-                );
-
-            employees.Add(
-                 new Employee()
-                 {
-                     Id = 2,
-                     FirstName = "Giorgi",
-                     LastName = "Giorgadze"
-                 }
-            );
-
-            employees.Add(
-                new Employee()
-                {
-                    Id = 3,
-                    FirstName = "Davit",
-                    LastName = "Davitidze"
-                }
-            );
-
-        }
+            new Employee
+            {
+                Id = 1,
+                FirstName = "Nika",
+                LastName = "Chkhartishvili"
+            },
+            new Employee
+            {
+                Id = 2,
+                FirstName = "Giorgi",
+                LastName = "Giorgadze"
+            },
+            new Employee
+            {
+                Id = 3,
+                FirstName = "Davit",
+                LastName = "Davitidze"
+            },
+        };
 
         [HttpGet]
         public List<Employee> GetEmployees()
         {
-            return employees;
+            return _employees;
         }
 
         [HttpGet("{id}")]
         public Employee GetEmployee(int id)
         {
-            var result = employees.Find(x => x.Id == id);
+            var result = _employees.Find(x => x.Id == id);
             return result;
         }
 
@@ -55,7 +45,7 @@ namespace Employees.API.Controllers
         [HttpGet("{lName}/{id}")]
         public Employee GetEmployeeWithDze(string lName, int id)
         {
-            var result = employees
+            var result = _employees
                 .Where(x => x.LastName.EndsWith(lName))
                 .FirstOrDefault(x => x.Id == id);
 
@@ -63,9 +53,26 @@ namespace Employees.API.Controllers
         }
 
         [HttpPost]
-        public void AddNewEmployee(Employee employee)
+        public List<Employee> AddNewEmployee(Employee employee)
         {
-            throw new NotImplementedException();
+            _employees.Add(employee);
+            return _employees;
+        }
+
+        [HttpDelete("{id}")]
+        public void DeleteEmployee(int id)
+        {
+            var employee = _employees.Find(x => x.Id == id);
+            _employees.Remove(employee);
+        }
+
+        [HttpPut]
+        public void UpdateEmployee(int id, Employee newEmployee)
+        {
+            var employeeToUpdate = _employees.Find(x => x.Id == id);
+
+            employeeToUpdate.FirstName = newEmployee.FirstName;
+            employeeToUpdate.LastName = newEmployee.LastName;
         }
     }
 }
