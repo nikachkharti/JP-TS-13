994ce5328f4017e77f8833843b2a0176fa40f52c
visual studio <vs@itstep.ge>
2023-11-23 17:45:41 +0400

    Companies table added

--- JP-TS-13/Employees.API/Data/ApplicationDbContext.cs
+++ JP-TS-13/Employees.API/Data/ApplicationDbContext.cs
@@ -9,5 +9,6 @@ namespace Employees.API.Data
         }
 
         public DbSet<Employee> Employees { get; set; }
+        public DbSet<Company> Companies { get; set; }
     }
 }
---
+++ JP-TS-13/Employees.API/Migrations/20231123134246_Companies.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Employees.API.Migrations
+{
+    public partial class Companies : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Companies",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreateDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Companies", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Companies");
+        }
+    }
+}
