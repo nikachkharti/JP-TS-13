e99431996c622b8b0bcfed65beb9564426ed2b88
visual studio <vs@itstep.ge>
2023-12-23 17:45:28 +0400

    File download

---
+++ JP-TS-13/Employees.API/Controllers/FileController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Employees.API.Controllers
+{
+    [Route("api/file")]
+    [ApiController]
+    public class FileController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetFile(string filename)
+        {
+            var path = Path.GetFullPath("./wwwroot/images/" + filename);
+
+            MemoryStream memory = new MemoryStream();
+            using (FileStream stream = new FileStream(path, FileMode.Open))
+            {
+                await stream.CopyToAsync(memory);
+            }
+            memory.Position = 0;
+            return File(memory, "image/png", Path.GetFileName(path));
+        }
+    }
+}
--- JP-TS-13/Employees.API/Program.cs
+++ JP-TS-13/Employees.API/Program.cs
@@ -35,6 +35,7 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseCors(MyAllowSpecificOrigins);
 app.UseAuthorization();
+app.UseStaticFiles();
 
 app.MapControllers();
 
